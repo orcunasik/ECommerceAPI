5dbdee21616bf7a3210977d813ccbe5e92fbece2
orcunasik <142536Orc>
2022-12-31 22:39:25 +0300

    While we create a migration, the DesignTime class was created to avoid errors in terminal such as PowerShell.

---
+++ Infrastructure/ECommerceAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ECommerceAPIDbContext>
+    {
+        public ECommerceAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
