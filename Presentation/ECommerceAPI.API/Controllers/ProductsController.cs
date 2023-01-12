d703e98cb17d48c5f6288009a360f3fb198e9622
Orçun Aşık <asikorcun@gmail.com>
2023-01-12 16:04:01 +0300

    Created a test Controller for the Product

---
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+        [HttpGet]
+        public async Task Get()
+        {
+             await _productWriteRepository.AddRangeAsync(new()
+            {
+                new(){Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock =15},
+                new(){Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock =10},
+                new(){Id = Guid.NewGuid(), Name = "Product 3", Price = 150, CreatedDate = DateTime.UtcNow, Stock =20},
+                new(){Id = Guid.NewGuid(), Name = "Product 4", Price = 250, CreatedDate = DateTime.UtcNow, Stock =25}
+            });
+        }
+    }
+}
